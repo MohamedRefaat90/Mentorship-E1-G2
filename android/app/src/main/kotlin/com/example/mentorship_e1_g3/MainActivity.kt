2884b15c007d3c4026c9bb68151e977fb818a6f4
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-03-28 00:07:11 +0200

    Init Commit

---
+++ android/app/src/main/kotlin/com/example/mentorship_e1_g3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mentorship_e1_g3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mentorship_e1_g3/features/home/view/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
