eb96932eb8998aa8bc9eb8210a781f4680a2c288
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-04-06 13:56:55 +0200

    launch custom app bar and finish launche screen

--- lib/features/home/presentation/pages/launches_screen.dart
+++ lib/features/home/presentation/pages/launches_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:mentorship_e1_g3/features/home/presentation/widgets/launch_item.dart';
 
+import '../widgets/custom_launch_app_bar.dart';
+
 class LaunchesScreen extends StatelessWidget {
   const LaunchesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return LaunchItem();
+    return Column(
+      children: [
+        const CustomLaunchAppBar(),
+        Expanded(
+          child: ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (BuildContext context, int index)=>const Padding(
+              padding:  EdgeInsets.all(12.0),
+              child: LaunchItem(),
+            ),
+            itemCount: 10,
+          ),
+        ),
+      ],
+    );
   }
 }
