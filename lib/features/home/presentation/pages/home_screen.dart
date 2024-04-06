8896231ff6793e50c266efa411b7b5cd26a66057
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-04-06 10:59:36 +0200

    bottom navigation bar and remove assets

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:mentorship_e1_g3/core/themes/app_pallete.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return  SafeArea(
+      child: Scaffold(
+        backgroundColor: AppPallete.homeBG,
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor:AppPallete.kPurple ,
+          unselectedItemColor: AppPallete.greyColor,
+          elevation: 11,
+          backgroundColor:AppPallete.homeBG,
+          items: const [
+              BottomNavigationBarItem(
+                icon:Icon(Icons.home_filled),
+                label:'Upcoming' ,
+              ),
+             BottomNavigationBarItem(
+                icon:Icon(Icons.history),
+                label:'Launches' ,
+              ),
+             BottomNavigationBarItem(
+                icon:Icon(Icons.rocket_launch),
+                label:'Rockets' ,
+              ),
+        ],)
+      ),
     );
   }
 }
