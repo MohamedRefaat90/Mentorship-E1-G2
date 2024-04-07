5c8e8bb48bce56ef91d8287be977b238ef04843c
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-04-07 22:08:48 +0200

    - add splash screen asstes

--- lib/core/resources/assets.dart
+++ lib/core/resources/assets.dart
@@ -3,5 +3,8 @@ class Assets {
 
   static const rocket = 'assets/images/home/rocket.png';
 
-
+  static const splashGalaxy = 'assets/images/splash/galaxy.png';
+  static const splashSpaceText = 'assets/images/splash/space_text.png';
+  static const splashSpaceman = 'assets/images/splash/spaceman.png';
+  static const splashBackground = 'assets/images/splash/splash_background.png';
 }
--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:mentorship_e1_g3/core/resources/assets.dart';
 
 import '../../core/routing/app_routing.dart';
 import '../Auth/login/presentation/pages/login_screen.dart';
@@ -18,33 +19,11 @@ class _SplashScreenState extends State<SplashScreen>
   late Animation _animation;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              AnimatedBuilder(
-                animation: _animation,
-                builder: (context, child) =>
-                    Transform.rotate(angle: _animation.value, child: child),
-                child: Center(
-                  child: Image.asset("assets/images/splash/galaxy.png"),
-                ),
-              ),
-              Center(
-                  child: Image.asset(
-                "assets/images/splash/spaceman.png",
-                width: 190,
-              )),
-            ],
-          ),
-          Image.asset("assets/images/splash/space_text.png")
-        ],
-      ),
-    );
+  void initState() {
+    initAnimationController();
+    setRotaionDegree(360);
+
+    super.initState();
   }
 
   initAnimationController() {
@@ -59,17 +38,39 @@ class _SplashScreenState extends State<SplashScreen>
     });
   }
 
-  @override
-  void initState() {
-    initAnimationController();
-    setRotaionDegree(360);
-
-    super.initState();
-  }
-
   setRotaionDegree(int degree) {
     final angle = degree * pi / 180;
     _animation =
         Tween<double>(begin: 0, end: angle).animate(_animationController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: _animation,
+                builder: (context, child) =>
+                    Transform.rotate(angle: _animation.value, child: child),
+                child: Center(
+                  child: Image.asset(Assets.splashGalaxy),
+                ),
+              ),
+              Center(
+                  child: Image.asset(
+                Assets.splashSpaceman,
+                width: 190,
+              )),
+            ],
+          ),
+          Image.asset(Assets.splashSpaceText)
+        ],
+      ),
+    );
   }
 }
