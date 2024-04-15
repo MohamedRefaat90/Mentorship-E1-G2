c9610e9ee7378e76901476a2a8b59b5de854dcc3
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-04-15 05:58:16 +0200

    open to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MentorshipE1G2 extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTthemeMode,
       navigatorKey: navigator,
-      home: SpaceXDetailsScreen(),
+      home: const LoginScreen(),
     );
   }
 }
