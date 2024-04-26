22f1044943d28392a9dd607e9526af8617172eb0
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-04-26 12:36:20 +0300

    - create LoginMethods interface

--- lib/features/Auth/login/cubit/login_cubit.dart
+++ lib/features/Auth/login/cubit/login_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:mentorship_e1_g3/features/Auth/login/cubit/login_methods.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
---
+++ lib/features/Auth/login/cubit/login_methods.dart
@@ -0,0 +1,13 @@
+abstract class LoginMethods {
+  login();
+}
+
+class GoogleLogin implements LoginMethods {
+  @override
+  login() {}
+}
+
+class PhoneLogin implements LoginMethods {
+  @override
+  login() {}
+}
