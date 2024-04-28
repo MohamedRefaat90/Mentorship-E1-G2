4f5398408ed3a4443ebe9afe514b669c2237ccba
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-04-28 15:14:57 +0300

    add the screen to nav bar

--- lib/features/home/presentation/widgets/bottom_nav_bar_items_list.dart
+++ lib/features/home/presentation/widgets/bottom_nav_bar_items_list.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
-List<BottomNavigationBarItem> items= const[
-    BottomNavigationBarItem(
-                icon:Icon(Icons.home_filled),
-                label:'Upcoming' ,
-              ),
-             BottomNavigationBarItem(
-                icon:Icon(Icons.history),
-                label:'Launches' ,
-              ),
-             BottomNavigationBarItem(
-                icon:Icon(Icons.rocket_launch),
-                label:'Rockets' ,
-              ),   
+List<BottomNavigationBarItem> items = const [
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home_filled),
+    label: 'Upcoming',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.history),
+    label: 'Launches',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.rocket_launch),
+    label: 'Rockets',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.people),
+    label: 'Crew',
+  ),
 ];
