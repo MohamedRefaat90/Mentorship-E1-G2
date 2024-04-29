f3122243fb79ae9b597bb4011d35dc2c24e466cb
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-04-29 16:05:57 +0300

    upcoming launches model

---
+++ lib/features/home/data/models/upcoming_launches/upcoming_launches_model.dart
@@ -0,0 +1,86 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'upcoming_launches_model.g.dart';
+
+@JsonSerializable()
+class UpcomingLaunches {
+  Links? links;
+  bool? net;
+  String? rocket;
+  List<String>? payloads;
+  String? launchpad;
+  int? flightNumber;
+  String? name;
+  String? dateUtc;
+  int? dateUnix;
+  String? dateLocal;
+  String? datePrecision;
+  bool? upcoming;
+  List<Cores>? cores;
+  bool? autoUpdate;
+  bool? tbd;
+  String? launchLibraryId;
+  String? id;
+
+  UpcomingLaunches(
+      {
+      this.links,
+      this.net,
+      this.rocket,
+      this.payloads,
+      this.launchpad,
+      this.flightNumber,
+      this.name,
+      this.dateUtc,
+      this.dateUnix,
+      this.dateLocal,
+      this.datePrecision,
+      this.upcoming,
+      this.cores,
+      this.autoUpdate,
+      this.tbd,
+      this.launchLibraryId,
+      this.id}); 
+
+  factory UpcomingLaunches.fromJson(Map<String, dynamic> json) =>
+    _$UpcomingLaunchesFromJson(json); 
+
+  Map<String, dynamic> toJson() => _$UpcomingLaunchesToJson(this); 
+   
+}
+
+@JsonSerializable()
+class Links {
+  String? webcast;
+  String? youtubeId;
+
+  Links(
+      {
+      this.webcast,
+      this.youtubeId,
+      });
+  factory Links.fromJson(Map<String, dynamic> json) =>
+    _$LinksFromJson(json); 
+
+  Map<String, dynamic> toJson() => _$LinksToJson(this);
+}
+
+@JsonSerializable()
+class Cores {
+  String? core;
+  int? flight;
+  bool? gridfins;
+  bool? legs;
+  bool? reused;
+
+  Cores(
+      {this.core,
+      this.flight,
+      this.gridfins,
+      this.legs,
+      this.reused,
+      });
+ factory Cores.fromJson(Map<String, dynamic> json) =>
+    _$CoresFromJson(json); 
+
+  Map<String, dynamic> toJson() => _$CoresToJson(this);
+}
---
+++ lib/features/home/data/models/upcoming_launches/upcoming_launches_model.g.dart
@@ -0,0 +1,81 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'upcoming_launches_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UpcomingLaunches _$UpcomingLaunchesFromJson(Map<String, dynamic> json) =>
+    UpcomingLaunches(
+      links: json['links'] == null
+          ? null
+          : Links.fromJson(json['links'] as Map<String, dynamic>),
+      net: json['net'] as bool?,
+      rocket: json['rocket'] as String?,
+      payloads: (json['payloads'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      launchpad: json['launchpad'] as String?,
+      flightNumber: (json['flightNumber'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      dateUtc: json['dateUtc'] as String?,
+      dateUnix: (json['dateUnix'] as num?)?.toInt(),
+      dateLocal: json['dateLocal'] as String?,
+      datePrecision: json['datePrecision'] as String?,
+      upcoming: json['upcoming'] as bool?,
+      cores: (json['cores'] as List<dynamic>?)
+          ?.map((e) => Cores.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      autoUpdate: json['autoUpdate'] as bool?,
+      tbd: json['tbd'] as bool?,
+      launchLibraryId: json['launchLibraryId'] as String?,
+      id: json['id'] as String?,
+    );
+
+Map<String, dynamic> _$UpcomingLaunchesToJson(UpcomingLaunches instance) =>
+    <String, dynamic>{
+      'links': instance.links,
+      'net': instance.net,
+      'rocket': instance.rocket,
+      'payloads': instance.payloads,
+      'launchpad': instance.launchpad,
+      'flightNumber': instance.flightNumber,
+      'name': instance.name,
+      'dateUtc': instance.dateUtc,
+      'dateUnix': instance.dateUnix,
+      'dateLocal': instance.dateLocal,
+      'datePrecision': instance.datePrecision,
+      'upcoming': instance.upcoming,
+      'cores': instance.cores,
+      'autoUpdate': instance.autoUpdate,
+      'tbd': instance.tbd,
+      'launchLibraryId': instance.launchLibraryId,
+      'id': instance.id,
+    };
+
+Links _$LinksFromJson(Map<String, dynamic> json) => Links(
+      webcast: json['webcast'] as String?,
+      youtubeId: json['youtubeId'] as String?,
+    );
+
+Map<String, dynamic> _$LinksToJson(Links instance) => <String, dynamic>{
+      'webcast': instance.webcast,
+      'youtubeId': instance.youtubeId,
+    };
+
+Cores _$CoresFromJson(Map<String, dynamic> json) => Cores(
+      core: json['core'] as String?,
+      flight: (json['flight'] as num?)?.toInt(),
+      gridfins: json['gridfins'] as bool?,
+      legs: json['legs'] as bool?,
+      reused: json['reused'] as bool?,
+    );
+
+Map<String, dynamic> _$CoresToJson(Cores instance) => <String, dynamic>{
+      'core': instance.core,
+      'flight': instance.flight,
+      'gridfins': instance.gridfins,
+      'legs': instance.legs,
+      'reused': instance.reused,
+    };
