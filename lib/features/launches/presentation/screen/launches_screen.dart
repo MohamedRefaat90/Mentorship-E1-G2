747bacaacec57794669d0c4db902d206d9b508ea
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-04-29 17:14:16 +0300

    get launches and upcoming launches data

--- lib/features/launches/presentation/screen/launches_screen.dart
+++ lib/features/launches/presentation/screen/launches_screen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentorship_e1_g3/core/widgets/launch_item.dart';
+import 'package:mentorship_e1_g3/features/home/logic/cubit/home_cubit.dart';
 
 import '../widgets/custom_launch_app_bar.dart';
 
-class LaunchesScreen extends StatelessWidget {
+class LaunchesScreen extends StatefulWidget {
   const LaunchesScreen({super.key});
 
+  @override
+  State<LaunchesScreen> createState() => _LaunchesScreenState();
+}
+
+class _LaunchesScreenState extends State<LaunchesScreen> {
+  @override
+   void initState() {
+    super.initState();
+    BlocProvider.of<HomeCubit>(context).emitGetLaunches();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
--- lib/features/upcoming/presentation/screen/upcoming_screen.dart
+++ lib/features/upcoming/presentation/screen/upcoming_screen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentorship_e1_g3/core/extension/num_extension.dart';
 import 'package:mentorship_e1_g3/core/themes/styles.dart';
 import 'package:mentorship_e1_g3/core/widgets/launch_item.dart';
+import 'package:mentorship_e1_g3/features/home/logic/cubit/home_cubit.dart';
 import 'package:mentorship_e1_g3/features/upcoming/presentation/widgets/upcoming_launch_details.dart';
 
 import '../widgets/count_down.dart';
 
-class UpcomingScreen extends StatelessWidget {
+class UpcomingScreen extends StatefulWidget {
   const UpcomingScreen({super.key});
 
+  @override
+  State<UpcomingScreen> createState() => _UpcomingScreenState();
+}
+
+class _UpcomingScreenState extends State<UpcomingScreen> {
+   @override
+   void initState() {
+    super.initState();
+    BlocProvider.of<HomeCubit>(context).emitGetUpcomingLaunches();
+  }
   @override
   Widget build(BuildContext context) {
     return  Padding(
