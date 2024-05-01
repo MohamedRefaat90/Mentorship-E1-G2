8dd2e8606b115f391b62e81b2b4e04ef1342e11c
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-05-01 07:43:45 +0300

    Merge branch 'Development' of https://github.com/MohamedRefaat90/Mentorship-E1-G2 into features/Static_deatils_screen_design

--- lib/features/home/logic/cubit/home_cubit.dart
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -1,27 +1,26 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mentorship_e1_g3/features/launches/presentation/screen/launches_screen.dart';
+import 'package:mentorship_e1_g3/core/di/dependency_injection.dart';
 import 'package:mentorship_e1_g3/features/rockets/presentation/screen/rockets_screen.dart';
+import 'package:mentorship_e1_g3/features/rockets/presentation/screen/rockets_screen.dart';
+import 'package:mentorship_e1_g3/features/launches/presentation/screen/launches_screen.dart';
 import 'package:mentorship_e1_g3/features/upcoming/presentation/screen/upcoming_screen.dart';
+
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
-  
-    int currentIndex = 0;
 
-    void changeBottomNavBar(int index){
-     currentIndex=index;
-     emit(ChangeBottomNavBar());
-   }
+  int currentIndex = 0;
 
-   List<Widget>homeScreens=const[
+  void changeBottomNavBar(int index) {
+    currentIndex = index;
+    emit(ChangeBottomNavBar());
+  }
+
+  List<Widget> homeScreens = const [
     UpcomingScreen(),
     LaunchesScreen(),
     RocketScreen(),
-   ];
-
-
-
+  ];
 }
---
+++ lib/features/rockets/ui/widgets/rocket_list_veiw.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RocketListVeiw extends StatelessWidget {
+  const RocketListVeiw({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: RocketListVeiw(),
+        );
+      },
+    );
+  }
+}
