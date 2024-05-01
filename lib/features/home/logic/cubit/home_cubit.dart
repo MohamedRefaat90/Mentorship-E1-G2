a4bc26196d2250623c290dfc9a33b2dd6206b01d
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-05-01 18:24:30 +0300

    - fix RocketScreen import in home cubit

--- lib/features/home/logic/cubit/home_cubit.dart
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:mentorship_e1_g3/features/crew/presentation/screens/crew_screen.dart';
-import 'package:mentorship_e1_g3/features/rockets/presentation/screen/rockets_screen.dart';
 import 'package:mentorship_e1_g3/features/launches/presentation/screen/launches_screen.dart';
 import 'package:mentorship_e1_g3/features/upcoming/presentation/screen/upcoming_screen.dart';
-import 'package:mentorship_e1_g3/core/di/dependency_injection.dart';
-import '../../../profile/presentation/pages/profile_screen.dart';
-import '../../../rockets/logic/cubit/rocket_cubit.dart';
 
+import '../../../profile/presentation/pages/profile_screen.dart';
+import '../../../rockets/ui/screen/rockets_screen.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
-
   int currentIndex = 0;
-
-  void changeBottomNavBar(int index) {
-    currentIndex = index;
-    emit(ChangeBottomNavBar());
-  }
-
 
   List<Widget> homeScreens = const [
     UpcomingScreen(),
@@ -29,6 +18,12 @@ class HomeCubit extends Cubit<HomeState> {
     RocketScreen(),
     CrewScreen(),
     ProfileScreen(),
-
   ];
+
+  HomeCubit() : super(HomeInitial());
+
+  void changeBottomNavBar(int index) {
+    currentIndex = index;
+    emit(ChangeBottomNavBar());
+  }
 }
