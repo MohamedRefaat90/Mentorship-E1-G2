21a945c1dfeff0f305e613cc38ab2d2029067478
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-05-03 17:55:20 +0300

    - New splash

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:mentorship_e1_g3/core/resources/assets.dart';
+import 'package:lottie/lottie.dart';
 
+import '../../core/helpers/functions/customLottieDecoder.dart';
 import '../../core/routing/app_routing.dart';
 import '../Auth/login/presentation/screen/login_screen.dart';
 
@@ -15,62 +14,19 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation _animation;
-
   @override
   void initState() {
-    initAnimationController();
-    setRotaionDegree(360);
-
     super.initState();
-  }
-
-  initAnimationController() {
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 3))
-          ..forward();
-
-    _animationController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        pushReplacement(const LoginScreen());
-      }
-    });
-  }
-
-  setRotaionDegree(int degree) {
-    final angle = degree * pi / 180;
-    _animation =
-        Tween<double>(begin: 0, end: angle).animate(_animationController);
+    Future.delayed(
+        const Duration(seconds: 6), () => pushReplacement(const LoginScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              AnimatedBuilder(
-                animation: _animation,
-                builder: (context, child) =>
-                    Transform.rotate(angle: _animation.value, child: child),
-                child: Center(
-                  child: Image.asset(Assets.splashGalaxy),
-                ),
-              ),
-              Center(
-                  child: Image.asset(
-                Assets.splashSpaceman,
-                width: 190,
-              )),
-            ],
-          ),
-          Image.asset(Assets.splashSpaceText)
-        ],
-      ),
-    );
+        backgroundColor: Colors.black,
+        body: Center(
+            child: Lottie.asset("assets/animations/splash/SpacexSplash.lottie",
+                decoder: customDecoder)));
   }
 }
