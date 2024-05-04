e926a994c78f686053aef802ea551507d676e848
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-05-04 05:18:00 +0300

    put the custom lottie widget on crew screen

--- lib/features/crew/presentation/screens/crew_screen.dart
+++ lib/features/crew/presentation/screens/crew_screen.dart
@@ -3,28 +3,27 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentorship_e1_g3/core/themes/styles.dart';
 import 'package:mentorship_e1_g3/core/themes/app_pallete.dart';
 import 'package:mentorship_e1_g3/core/di/dependency_injection.dart';
+import 'package:mentorship_e1_g3/core/widgets/custom_error_widget.dart';
+import 'package:mentorship_e1_g3/core/widgets/custom_loading_widget.dart';
 import 'package:mentorship_e1_g3/features/crew/data/model/crew_model.dart';
 import 'package:mentorship_e1_g3/features/crew/logic/cubit/crew_cubit.dart';
 import 'package:mentorship_e1_g3/features/crew/logic/cubit/crew_state.dart';
 import 'package:mentorship_e1_g3/features/crew/presentation/widgets/crew_screen_widgets.dart';
 
-
-
 class CrewScreen extends StatelessWidget {
   const CrewScreen({Key? key}) : super(key: key);
 
-  @override  Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppPalette.homeBG,
       appBar: AppBar(
         backgroundColor: AppPalette.homeBG,
-
         title: Text(
           'Crew Members',
           style: AppStyles.font24BoldWhite(context),
         ),
       ),
-
       body: BlocProvider(
         create: (context) => getIt<CrewCubit>()..getAllCrew(),
         child: CrewScreenBody(),
@@ -43,15 +42,12 @@ class CrewScreenBody extends StatelessWidget {
             child: CircularProgressIndicator(),
           ),
           loading: () => Center(
-            child: CircularProgressIndicator(),
+            child: CustomLoadingWidget(),
           ),
           success: (crewList) => CrewGrid(allCrew: crewList),
-          error: (error) => Center(
-            child: Text('Error: $error'),
-          ),
+          error: (error) => const CustomErrorWidget(),
         );
       },
     );
-
   }
 }
