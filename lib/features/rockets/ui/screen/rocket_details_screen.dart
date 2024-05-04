8641fe0cf6b6724d0b22eeaf24423ddf54d54585
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-05-04 19:31:30 +0300

    use our styles

--- lib/features/rockets/ui/screen/rocket_details_screen.dart
+++ lib/features/rockets/ui/screen/rocket_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:mentorship_e1_g3/core/themes/styles.dart';
 import 'package:mentorship_e1_g3/features/rockets/data/models/rocket_respons_body.dart';
 
 class RocketDetailsScreen extends StatelessWidget {
@@ -25,67 +26,65 @@ class RocketDetailsScreen extends StatelessWidget {
                   aspectRatio: 16 / 9,
                   enlargeCenterPage: true,
                   autoPlay: true, // Auto play set to true
-                  autoPlayInterval: Duration(
-                      seconds: 3), // Auto play interval set to 3 seconds
+                  autoPlayInterval: Duration(seconds: 3), // Auto play interval set to 3 seconds
                   autoPlayAnimationDuration: Duration(milliseconds: 800),
                   autoPlayCurve: Curves.fastOutSlowIn,
                   enableInfiniteScroll: false,
                 ),
                 items: rocket.flickrImages?.map((image) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                image: NetworkImage(image),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          );
-                        },
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: NetworkImage(image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       );
-                    }).toList() ??
-                    [],
+                    },
+                  );
+                }).toList() ?? [],
               ),
             ),
             SizedBox(height: 20.0),
             Text(
               'Description:',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font24BoldWhite(context),
             ),
             SizedBox(height: 10.0),
             Text(
               rocket.description ?? 'No description available',
-              style: TextStyle(fontSize: 16.0),
+              style: AppStyles.font15MediumGrey(context),
             ),
             SizedBox(height: 20.0),
             Text(
               'Height: ${rocket.height?.meters} meters / ${rocket.height?.feet} feet',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font18SemiBoldWhite(context),
             ),
             SizedBox(height: 10.0),
             Text(
               'Diameter: ${rocket.diameter?.meters} meters / ${rocket.diameter?.feet} feet',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font18SemiBoldWhite(context),
             ),
             SizedBox(height: 10.0),
             Text(
               'Mass: ${rocket.mass?.kg} kg / ${rocket.mass?.lb} lb',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font18SemiBoldWhite(context),
             ),
             SizedBox(height: 10.0),
             Text(
               'First Stage Engines: ${rocket.firstStage?.engines}',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font18SemiBoldWhite(context),
             ),
             SizedBox(height: 10.0),
             Text(
               'Second Stage Engines: ${rocket.secondStage?.engines}',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: AppStyles.font18SemiBoldWhite(context),
             ),
             // Add more details as needed
           ],
