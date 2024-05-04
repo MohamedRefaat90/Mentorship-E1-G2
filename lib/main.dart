a1a1ee3874e2eafbe2dd449ee29e83f2732ba2fb
AMIRAMOHMED <amiramohmaed887@gmail.com>
2024-05-04 06:49:59 +0300

    added option connectivity to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+import 'package:mentorship_e1_g3/core/screens/no_net_working_screen.dart';
 import 'package:mentorship_e1_g3/core/themes/app_theme.dart';
 import 'package:mentorship_e1_g3/features/Auth/login/cubit/login_cubit.dart';
+import 'package:mentorship_e1_g3/features/Auth/login/presentation/screen/login_screen.dart';
+import 'package:mentorship_e1_g3/features/home/presentation/screen/home_screen.dart';
 
 import 'core/services/initServices.dart';
 import 'features/splash/splash_screen.dart';
@@ -19,14 +23,27 @@ class MentorshipE1G2 extends StatelessWidget {
   const MentorshipE1G2({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LoginCubit(),
-      child: MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: AppTheme.darkTthemeMode,
         navigatorKey: navigator,
-        home: const SplashScreen(),
-      ),
-    );
+        home: OfflineBuilder(
+          connectivityBuilder: (
+            BuildContext context,
+            ConnectivityResult connectivity,
+            Widget child,
+          ) {
+            final bool connected = connectivity != ConnectivityResult.none;
+            if (connected) {
+              return BlocProvider(
+                create: (context) => LoginCubit(),
+                child: const SplashScreen(),
+              );
+            } else {
+              return const NoNetWorkScreen();
+            }
+          },
+          child: Container(),
+        ));
   }
 }
