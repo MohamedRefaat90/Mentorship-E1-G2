53b893d5f57a035c2c4858b4a55391153600f4d2
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-05-04 19:06:12 +0300

    put the base of screen

--- lib/features/rockets/ui/screen/rocket_details_screen.dart
+++ lib/features/rockets/ui/screen/rocket_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:mentorship_e1_g3/features/rockets/data/models/rocket_respons_body.dart';
 
 class RocketDetailsScreen extends StatelessWidget {
@@ -17,6 +18,51 @@ class RocketDetailsScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 200,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 16 / 9,
+                  enlargeCenterPage: true,
+                  autoPlay: true, // Auto play set to true
+                  autoPlayInterval: Duration(
+                      seconds: 3), // Auto play interval set to 3 seconds
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: false,
+                ),
+                items: rocket.flickrImages?.map((image) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(8.0),
+                              image: DecorationImage(
+                                image: NetworkImage(image),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList() ??
+                    [],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Description:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              rocket.description ?? 'No description available',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20.0),
             Text(
               'Height: ${rocket.height?.meters} meters / ${rocket.height?.feet} feet',
               style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
